8350e818155b706b3f152459d6fe49d2b73fcf03
Jamparaju Nammi <bharaninammi@cometnet-10-21-11-14.utdallas.edu>
2018-02-25 02:59:36 -0600

    Initial Commit

---
+++ beatSurya/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  beatSurya
+//
+//  Created by Bharani Nammi on 2/25/18.
+//  Copyright © 2018 Bharani Nammi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
